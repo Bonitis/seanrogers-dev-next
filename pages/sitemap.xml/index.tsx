18b461c257b3a48b9031aa81a3207e31608662c7
Sean Rogers <smr04003@gmail.com>
2022-02-06 12:07:04 -0500

    add sitemap

---
+++ pages/sitemap.xml/index.tsx
@@ -0,0 +1,65 @@
+import { GetServerSideProps } from 'next';
+
+const Sitemap = () => {
+    return null;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+
+<url>
+    <loc>https://seanrogers.dev/</loc>
+    <lastmod>2022-02-06T16:52:08+00:00</lastmod>
+    <priority>1.00</priority>
+</url>
+<url>
+    <loc>https://seanrogers.dev/projects/smpl</loc>
+    <lastmod>2022-02-06T16:52:08+00:00</lastmod>
+    <priority>0.80</priority>
+</url>
+<url>
+    <loc>https://seanrogers.dev/projects/ekos-order-hub</loc>
+    <lastmod>2022-02-06T16:52:08+00:00</lastmod>
+    <priority>0.80</priority>
+</url>
+<url>
+    <loc>https://seanrogers.dev/projects/ekos-boost</loc>
+    <lastmod>2022-02-06T16:52:08+00:00</lastmod>
+    <priority>0.80</priority>
+</url>
+<url>
+    <loc>https://seanrogers.dev/projects/takeshape</loc>
+    <lastmod>2022-02-06T16:52:08+00:00</lastmod>
+    <priority>0.80</priority>
+</url>
+<url>
+    <loc>https://seanrogers.dev/projects/push-the-pace</loc>
+    <lastmod>2022-02-06T16:52:08+00:00</lastmod>
+    <priority>0.80</priority>
+</url>
+<url>
+    <loc>https://seanrogers.dev/projects/the-wed-clique</loc>
+    <lastmod>2022-02-06T16:52:08+00:00</lastmod>
+    <priority>0.80</priority>
+</url>
+
+
+</urlset>
+    `;
+
+    res.setHeader('Content-Type', 'text/xml');
+    res.write(sitemap);
+    res.end();
+
+    return {
+        props: {},
+    };
+};
+
+export default Sitemap;
